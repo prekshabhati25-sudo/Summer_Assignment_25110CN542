ca89fd33f280a9da7a375b211115e3aec0ef98ac
PREKSHA BHATI <prekshabhati25@gmail.com>
2026-06-09 09:12:30 +0530

    Day-9

---
+++ Day-9/ques35.java
@@ -0,0 +1,14 @@
+public class ques35 
+{
+    public static void main(String args[])
+    {
+        for(int i=65;i<=69;i++)
+        {
+            for(int j=65;j<=i;j++)
+            {
+            System.out.print((char)i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
